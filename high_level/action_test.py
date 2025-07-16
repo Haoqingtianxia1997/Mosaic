2bc91c3d660d18b995a2b3c1cf9fb8306cb8462c
Charles Fu <charles940316@gmail.com>
2025-07-16 14:06:48 +0200

    add icp and point cloud filtering

--- high_level/action_test.py
+++ high_level/action_test.py
@@ -5,8 +5,8 @@ from execute.actions import execute_action_sequence
 
 if __name__ == "__main__":
     actions = [
-        {"type": "perceive", "target": "cup", "parameters": {}},
-        {"type": "move", "target": "cup","parameters": {}},
+        {"type": "perceive", "target": "tomato", "parameters": {}},
+        {"type": "move", "target": "tomato","parameters": {}},
         # {"type": "grasp_otherthings", "target": "tomato", "parameters": {}},
         # {"type": "perceive", "target": "user person", "parameters": {}},
         # {"type": "move", "target": "user person", "parameters": {}},
